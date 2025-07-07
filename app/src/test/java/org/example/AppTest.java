21273e65cfabf055c7d5c34463414a1238b1678c
Hordii125 <gordiysivokon2@gmail.com>
2025-07-06 20:46:35 -0500

    second commit, test are passing and all working fine

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -2,10 +2,55 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
 
 class AppTest {
   @Test
   void theTestsWork() {
     assertEquals(true, true);
+  }
+
+  @Test
+  void testLinearSearch() {
+    FuzzyListGenerator generator = new FuzzyListGenerator();
+    FuzzyFinder finder = new FuzzyFinder();
+    
+    ArrayList<Fuzzy> sortedFuzzies = generator.sortedRainbowFuzzies();
+    int count = finder.linearSearch(sortedFuzzies);
+    
+    assertTrue(count == 1, "Linear search should find exactly one 'gold' fuzzy in sorted list");
+  }
+
+  @Test
+  void testBinarySearch() {
+    FuzzyListGenerator generator = new FuzzyListGenerator();
+    FuzzyFinder finder = new FuzzyFinder();
+    
+    ArrayList<Fuzzy> sortedFuzzies = generator.sortedRainbowFuzzies();
+    int count = finder.binarySearch(sortedFuzzies);
+    
+    assertTrue(count == 1, "Binary search should find exactly one 'gold' fuzzy in sorted list");
+  }
+
+  @Test
+  void testLinearSearchRandom() {
+    FuzzyListGenerator generator = new FuzzyListGenerator();
+    FuzzyFinder finder = new FuzzyFinder();
+    
+    ArrayList<Fuzzy> randomFuzzies = generator.randomizedRainbowFuzzies();
+    int count = finder.linearSearch(randomFuzzies);
+    
+    assertTrue(count == 1, "Linear search should find exactly one 'gold' fuzzy in random list");
+  }
+
+  @Test
+  void testBinarySearchRandom() {
+    FuzzyListGenerator generator = new FuzzyListGenerator();
+    FuzzyFinder finder = new FuzzyFinder();
+    
+    ArrayList<Fuzzy> randomFuzzies = generator.randomizedRainbowFuzzies();
+    int count = finder.binarySearch(randomFuzzies);
+    
+    assertTrue(count == 0, "Binary search should find no 'gold' fuzzy in random list");
   }
 }
